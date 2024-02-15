53d9b5d7aff99c742a497f2ed08def2119ee0428
abernatskiy <abernatskiy@toha.li>
2024-02-15 22:46:03 +0900

    New processor interface
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -6,20 +6,18 @@ import { lookupArchive } from '@subsquid/archive-registry'
 export const GRAVATAR_CONTRACT = '0x2E645469f354BB4F5c8a05B3b30A929361cf77eC'.toLowerCase()
 
 export const processor = new EvmBatchProcessor()
-  .setDataSource({
-    // Lookup archive by the network name in Subsquid registry
-    // See https://docs.subsquid.io/evm-indexing/supported-networks/
-    archive: lookupArchive('eth-mainnet'),
-    // Chain RPC endpoint is required for
-    //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
-    //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
-    chain: {
-      // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
-      // https://docs.subsquid.io/deploy-squid/env-variables
-      url: assertNotNull(process.env.RPC_ENDPOINT),
-      // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
-      rateLimit: 10
-    }
+  // Lookup archive by the network name in Subsquid registry
+  // See https://docs.subsquid.io/evm-indexing/supported-networks/
+  .setGateway(lookupArchive('eth-mainnet'))
+  // Chain RPC endpoint is required for
+  //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
+  //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
+  .setRpcEndpoint({
+    // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
+    // https://docs.subsquid.io/deploy-squid/env-variables
+    url: assertNotNull(process.env.RPC_ENDPOINT),
+    // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
+    rateLimit: 10
   })
   .setBlockRange({ from: 6175243 })
   .setFinalityConfirmation(75)
